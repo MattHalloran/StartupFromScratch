cc450139deba424937232904fc2fbdaf274dc057
MattHalloran <matthalloran8@gmail.com>
2025-04-22 20:12:05 +0000

    develop.sh rewrite p4

--- scripts/develop/target/dockerOnly.sh
+++ scripts/develop/target/dockerOnly.sh
@@ -2,11 +2,21 @@
 
 ORIGINAL_DIR=$(pwd)
 HERE=$(cd "$(dirname \"${BASH_SOURCE[0]}\")" && pwd)
+COMPOSE_DIR=$(cd "$HERE"/../../.. && pwd)
 source "${HERE}/../../utils/index.sh"
 
 start_development_docker_only() {
     header "🚀 Starting Docker only development environment..."
+    cd "$COMPOSE_DIR"
 
+    cleanup() {
+        info "🔧 Cleaning up development environment at $COMPOSE_DIR..."
+        cd "$COMPOSE_DIR"
+        docker-compose down
+        cd "$ORIGINAL_DIR"
+        exit 0
+    }
+    trap cleanup SIGINT SIGTERM
     info "Starting all services in detached mode (Postgres, Redis, server, jobs, UI)..."
     docker-compose up -d
 
