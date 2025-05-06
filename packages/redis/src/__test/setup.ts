84880ce8af263f764e2e3c4791535560f2020f65
MattHalloran <matthalloran8@gmail.com>
2025-05-06 01:14:48 +0000

    redis package not needed

--- packages/redis/src/__test/setup.ts
+++
@@ -1,8 +0,0 @@
-import chai from 'chai';
-import sinon from 'sinon';
-
-// Expose chai expect and sinon globally for tests
-globalThis.expect = chai.expect;
-globalThis.sinon = sinon;
-
-export {}; 
--- packages/redis/src/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import client from './index';
-
-describe('Redis DB package', () => {
-  it('exports a connected client instance', () => {
-    expect(client).to.be.ok;
-  });
-}); 
